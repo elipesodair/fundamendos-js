794a18b43442249d0cfd84783ea31696c27b31c7
unknown <elipesodair0512@gmail.com>
2021-07-16 15:07:42 -0300

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,4 @@
+const numero = 381;
+const numeroSting = Number('360');
+
+console.log(numero + numeroSting);
---
+++ truthy-falsy.js
@@ -0,0 +1,19 @@
+//boolean
+const usuarioLogado = true
+const contapaga = false
+
+//truthy ou falsy
+//0 => false
+//1 => true 
+//console.log(0 == false);
+//console.log(1 == true);
+
+//let minhaVar =
+let varNull = null;
+ //console.log(minahVar);
+ console.log(varNull);
+
+ let numero = 3;
+ let texto = 'Aula';
+ console.log(typeof numero);
+ console.log(typeof texto);
