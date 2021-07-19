a9d09138e63b87f3d0163a15c6683dc5ada74eea
unknown <elipesodair0512@gmail.com>
2021-07-19 19:04:13 -0300

    templaite strings

---
+++ templaite-strings.js
@@ -0,0 +1,10 @@
+const nome = "Ju";
+const idade = 2021-1981;
+const cidadeDenacimento = "São Paulo";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " +
+//idade + ", e nasci na cidade de " + cidadeDenacimento;
+
+const apresentacao = `nome é ${nome}, minha idade é ${idade} e masci na cidade de ${cidadeDenacimento}`;
+
+console.log(apresentacao);
