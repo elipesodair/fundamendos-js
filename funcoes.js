abce2f3d05fd529571f7462e2424e1ae25d33323
unknown <elipesodair0512@gmail.com>
2021-07-20 10:40:33 -0300

    Funcões return

---
+++ funcoes.js
@@ -0,0 +1,49 @@
+//let x = "";
+//console.log(x);
+//x = "oi"
+function operacaoMatematica(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3
+   }
+   
+   operacaoMatematica(15, 30, 45) // 90
+  
+
+ function imprimeTexto(texto){
+    console.log(texto);
+}
+
+/* imprimeTexto("oi")
+imprimeTexto("Elipes")
+imprimeTexto("junior")
+
+ */ 
+imprimeTexto(soma());
+function soma (){
+    return resultado = 2+2;
+}
+
+//console.log(soma()); 
+function cumprimentar(){
+    console.log('oi gente!')
+   }
+   
+   cumprimentar()
+
+
+   function cumprimentaPessoa(pessoa){
+    console.log(`oi, ${pessoa}!`)
+   }
+   
+   cumprimentaPessoa('Helena')
+
+   function cumprimentar(){
+    return 'Oi gente!'
+   }
+   
+   function cumprimentaPessoa(nomePessoa) {
+    console.log(`${cumprimentar()} Meu nome é ${nomePessoa}`)
+   }
+   
+   cumprimentaPessoa('Paula') // “Oi gente! Meu nome é Paula”
+
+  
