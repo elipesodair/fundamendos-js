43db7b94dde3de9dbf75fb7501488c545179c972
unknown <elipesodair0512@gmail.com>
2021-07-21 14:38:11 -0300

    EXPRECOES

--- exprecoes.js
+++ exprecoes.js
@@ -1,5 +1,17 @@
-const soma = function(nun1, nun2){
-    return nun1 +nun2
-    
-}
+const soma = function(nun1, nun2){return nun1 +nun2}
 console.log(soma(1, 1));
+
+const escreveNome = function( priNome, sobreNome ) { return priNome + sobreNome}
+console.log(escreveNome("Elipes ", "Junior"));
+
+const lojaDeCarros = function ( carros, motos, caminhao){
+    return carros  +  motos + caminhao
+}
+console.log(lojaDeCarros("celta , corsa , uno , tipo , stilo , vectra , omega , fusca" , 
+"cg , dt, rs, xt, bross, cbr, falcom , tuwister, ybr", 
+"iveco , vw , ford , volvo, mercedes"));
+
+const lista = function(nome){
+    return nome
+}
+console.log(lista("elipes , Erica, Eliel, Eva, Eli, Emerson"));
